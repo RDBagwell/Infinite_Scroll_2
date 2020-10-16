b0cecf252a4bd86cde589f23858af87f72dd850f
Robert Bagwell <erbagwell@yahoo.com>
2020-10-15 23:26:17 -0600

    Add JS

--- js/script.js
+++ js/script.js
@@ -12,15 +12,60 @@ async function getPost() {
 }
 
 async function showPost() {
-    const data = await getPost();
-    const posts = data.map(post => `
-        <div class="post">
-            <div class="number">${post.id}</div>
-            <h2 class="post-title">${post.title}</h2>
-            <p class="post-body">${post.body}</p>   
+    const posts = await getPost();
+
+    posts.forEach( post  => {
+        const postEl = document.createElement('div');
+        postEl.classList.add('post'); 
+        postEl.innerHTML = `
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+          <h2 class="post-title">${post.title}</h2>
+          <p class="post-body">${post.body}</p>
         </div>
-    `).join('');
-    postContainer.innerHTML = posts;
+      `;
+      postContainer.appendChild(postEl);
+    });
+
 }
+
+function showLoader(){
+    loader.classList.add('show');
+    setTimeout(()=>{
+        loader.classList.remove('show');
+        setTimeout(()=>{
+            page++
+            showPost()
+        }, 300);
+    }, 1000);
+}
+
+function filterPost(e){
+    const term = e.target.value.toUpperCase();
+    const posts = document.querySelectorAll('.post');
+
+    posts.forEach((post)=>{
+        const title = post.querySelector('.post-title').textContent.toUpperCase();
+        const body = post.querySelector('.post-body').textContent.toUpperCase();
+
+        if(title.indexOf(term) > -1 || body.indexOf(term) > -1){
+            post.style.display = 'flex';
+        } else {
+            post.style.display = 'none'; 
+        }
+    });
+}
+
+
+window.addEventListener('scroll', ()=>{
+    const { scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+    if(scrollTop + clientHeight >= scrollHeight - 5 ){
+        showLoader()
+    }
+    
+});
+
+filter.addEventListener('input', filterPost)
 
 showPost();
